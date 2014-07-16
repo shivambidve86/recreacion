e50be18a56c8829308d103bf272660406337710f
shivambidve86 <shivambidve@gmail.com>
2014-07-16 23:21:30 +0100

    adding config files

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,18 +1,38 @@
 # config valid only for Capistrano 3.1
 lock '3.2.1'
 
-set :application, 'recreacion'
-set :repo_url, 'git@github.com:shivambidve86/recreacion.git'
+set :application, 'my_app_name'
+set :repo_url, 'git@example.com:me/my_repo.git'
 
-# describe the rbenv environment we are deploying into
-set :rbenv_type, :user
-set :rbenv_ruby, '2.0.0-p451'
-set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+# Default branch is :master
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
-# dirs we want symlinked to the shared folder
-# during deployment
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+# Default deploy_to directory is /var/www/my_app
+# set :deploy_to, '/var/www/my_app'
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# set :linked_files, %w{config/database.yml}
+
+# Default value for linked_dirs is []
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
 
 namespace :deploy do
 
@@ -21,9 +41,6 @@ namespace :deploy do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
       # execute :touch, release_path.join('tmp/restart.txt')
-      #
-      # The capistrano-unicorn-nginx gem handles all this
-      # for this example
     end
   end
 
