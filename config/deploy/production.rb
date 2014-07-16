061c9b3b2e7c4f252afb7f2a094c973539427cfd
shivambidve86 <shivambidve@gmail.com>
2014-07-16 23:56:52 +0100

    adding config files

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,10 @@
+set :user, 'shivazurevm'
+
+set :ssh_options, {
+    # verbose: :debug,
+    user: fetch(:user)
+}
+
 # production deployment
 set :stage, :production
 # use the master branch of the repository
@@ -8,8 +15,10 @@ set :deploy_user, "shivazurevm"
 # the 'full name' of the application
 set :full_app_name, "#{fetch(:application)}_#{fetch(:stage)}"
 # the server(s) to deploy to
-server 'shivazurevm.cloudapp.net', user: 'shivazurevm:@Shivam@86', roles: %w{web app db}, primary: true
+server 'shivazurevm.cloudapp.net', user: 'shivazurevm', roles: %w{web app db}, primary: true
 # the path to deploy to
 set :deploy_to, "/home/#{fetch(:deploy_user)}/apps/#{fetch(:full_app_name)}"
 # set to production for Rails
 set :rails_env, :production
+
+
